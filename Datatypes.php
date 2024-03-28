69efa31127b37296481babf1c4b352847e9695db
Ashton Mcdonald <110925432+ashtonm33@users.noreply.github.com>
2024-03-28 08:26:27 -0500

    u890y8

---
+++ Datatypes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    //string
+    $name= 'Coding is fun';
+
+    //Integer
+    $name= 20;
+    //Float
+    $name= 20.5784;
+    //Boolean
+
+    //Array
+    $names = array("joe","Dom","Mike");
+    echo $names['1'];
+    ?>
+</body>
+</html>
